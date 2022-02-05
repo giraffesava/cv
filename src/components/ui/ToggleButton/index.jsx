9c4595ccb0c8831f6aa5f30ec5e69aab67f830da
Sava <savely54@mail.ru>
2022-02-06 03:27:50 +0700

    added theme fixes

--- src/components/ui/ToggleButton/index.jsx
+++ src/components/ui/ToggleButton/index.jsx
@@ -3,20 +3,20 @@ import "./index.scss";
 import { useTheme } from "../../../hooks/use-theme";
 
 const ToggleButton = () => {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   const change = (e) => {
     if (e.target.checked) {
-      setTheme("dark");
+      setTheme("light");
       return;
     }
-    setTheme("light");
+    setTheme("dark");
   };
 
   return (
     <>
       <label className="switch">
-        <input type="checkbox" onChange={change} />
+        <input type="checkbox" checked={theme === "light"} onChange={change} />
         <span className="slider round"></span>
       </label>
     </>
