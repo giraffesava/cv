c1c094313509ee37dd0fbde1fe696aea5b8aed48
Sava <savely54@mail.ru>
2022-02-06 02:41:45 +0700

    added toggle switch

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,14 @@
 import React from "react";
-
+import ToggleButton from "../ui/ToggleButton";
 import "./header.scss";
 export const Header = () => {
-  return <div className="header">Here will be my cv soon</div>;
+  return (
+    <div className="header">
+      <div className="header-logo">Sava's website</div>
+      <div className="header-buttons">
+        <ToggleButton />
+        <div>EN</div>
+      </div>
+    </div>
+  );
 };
