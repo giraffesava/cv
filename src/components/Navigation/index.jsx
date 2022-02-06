2d64a9e14c13cf360eebdb0c30b890d99c3343af
Sava <savely54@mail.ru>
2022-02-06 19:29:42 +0700

    added fixes

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Image from "../Image";
+import NavigationMenu from "../NavigationMenu";
 import "./styles.scss";
 
 const Navigation = () => {
   return (
     <div className="navigation">
       <Image />
+      <h1>Frontend Developer</h1>
+      <NavigationMenu />
     </div>
   );
 };
