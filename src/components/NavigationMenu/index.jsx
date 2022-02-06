9e7004d35fa010a0ae125d12a1b6e76ae1fd09c2
Sava <savely54@mail.ru>
2022-02-06 19:29:35 +0700

    added navigation menu

---
+++ src/components/NavigationMenu/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavigationMenu = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <h1>Hard Skills</h1>
+      <h1>Soft Skills</h1>
+      <h1>Experience</h1>
+    </div>
+  );
+};
+
+export default NavigationMenu;
