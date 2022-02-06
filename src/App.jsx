82a1695edb874a60592717132bdb7aa76f8abce5
Sava <savely54@mail.ru>
2022-02-06 19:29:06 +0700

    Added cursor styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import "./App.scss";
 import Main from "./components/Main";
+//import Cursor from "./components/ui/Cursor";
 
 const App = () => {
   return (
     <div className="App">
+      {/* <Cursor /> */}
       <Header />
       <Main />
     </div>
---
+++ src/components/ui/Cursor/index.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef } from "react";
+import "./styles.scss";
+
+const Cursor = () => {
+  const cursorRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      const mouseX = clientX - cursorRef.current.clientWidth / 2; // or just e.pageX, e.pageY
+      const mouseY = clientY - cursorRef.current.clientHeight / 2;
+      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    });
+  }, []);
+  return <div className="cursor" ref={cursorRef} />;
+};
+
+export default Cursor;
