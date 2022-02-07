1b7293d067298c7626d45683ebef6cac1e29d9da
Sava <savely54@mail.ru>
2022-02-08 00:00:35 +0700

    added i18next

---
+++ src/i18next/i18next.js
@@ -0,0 +1,22 @@
+import translationsEn from "./locales/en/en.json";
+import translationsRu from "./locales/ru/ru.json";
+
+export const intl = {
+  // the translations
+  // (tip move them in a JSON file and import them,
+  // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+  resources: {
+    en: {
+      translation: translationsEn,
+    },
+    rus: {
+      translation: translationsRu,
+    },
+  },
+  lng: "en", // if you're using a language detector, do not define the lng option
+  fallbackLng: "en",
+
+  interpolation: {
+    escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+  },
+};
