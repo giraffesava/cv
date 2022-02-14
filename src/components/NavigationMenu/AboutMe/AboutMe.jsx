d776339d4dde9a4844c71d391a1ddb3ec25e7d60
Sava <savely54@mail.ru>
2022-02-14 23:44:52 +0700

    adding information about me

--- src/components/NavigationMenu/AboutMe/AboutMe.jsx
+++ src/components/NavigationMenu/AboutMe/AboutMe.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const AboutMe = () => {
-  return <div>Information about me</div>;
+  const { t } = useTranslation();
+  return (
+    <div className="about">
+      <ul>
+        <li>
+          {t("aboutMePage.age.title")}: {t("aboutMePage.age.value")}
+        </li>
+
+        <li>
+          {t("aboutMePage.name.title")}: {t("aboutMePage.name.value")}
+        </li>
+        <li>
+          {t("aboutMePage.interests.title")}: {t("aboutMePage.interests.value")}
+        </li>
+        <li>
+          {t("aboutMePage.education.title")}: {t("aboutMePage.education.value")}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default AboutMe;
--- src/components/NavigationMenu/HardSkills/HardSkills.jsx
+++ src/components/NavigationMenu/HardSkills/HardSkills.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const HardSkills = () => {
-  return <div>Hard skills</div>;
+  const { t } = useTranslation();
+
+  return (
+    <div className="hard-skills">
+      <h3>{t("hardSkillsPage.0")}</h3>
+      <h3>{t("hardSkillsPage.1")}</h3>
+      <ul>
+        <li>{t("hardSkillsPage.2")}</li>
+        <li>{t("hardSkillsPage.3")}</li>
+        <li>{t("hardSkillsPage.4")}</li>
+        <li>{t("hardSkillsPage.5")}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default HardSkills;
